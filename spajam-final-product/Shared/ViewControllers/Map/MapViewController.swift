e127cf980fe2e46a7decea879af6a61fdd538474
Hiroya NAKAJIMA <hiroya.nakajima@gmail.com>
2020-11-07 21:25:29 +0900

    mapView追加

--- spajam-final-product/Shared/ViewControllers/Map/MapViewController.swift
+++ spajam-final-product/Shared/ViewControllers/Map/MapViewController.swift
@@ -6,8 +6,15 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
     
     init() {
         super.init(nibName: nil, bundle: Bundle(for: type(of: self)))
@@ -17,22 +24,48 @@ class MapViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager!.requestWhenInUseAuthorization()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setCurrentLocation() {
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: span)
+        mapView.region = region
     }
-    */
+    
+    func getData() {
+        let action = DataGateway.getAction(GetShopsDataGatewayAction.self)
+        
+        action.elements.subscribe { shop in
+            shop.
+        }.disposed(by: disposeBag)
 
+    }
+    
+    @IBAction func currentButtonTap() {
+        setCurrentLocation()
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+            setCurrentLocation()
+            break
+        default:
+            break
+        }
+    }
 }
