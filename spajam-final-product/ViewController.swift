0d0539d43dd7676b64836f7d29afdbfbbbd0245b
Hiroya NAKAJIMA <hiroya.nakajima@gmail.com>
2020-11-07 19:30:01 +0900

    xib対応

--- spajam-final-product/ViewController.swift
+++ spajam-final-product/ViewController.swift
@@ -11,11 +11,26 @@ import RxMoya
 import Moya
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    init() {
+        super.init(nibName: nil, bundle: Bundle(for: type(of: self)))
+    }
+    
+    override func loadView() {
+        guard let view = UINib(nibName: Self.className, bundle: nil).instantiate(withOwner: self, options: nil).first as? UIView else {
+            return
+        }
+        self.view = view
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
 
 }
-
