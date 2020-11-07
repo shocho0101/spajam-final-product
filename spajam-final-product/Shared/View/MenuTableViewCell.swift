f5ef8d9b2ca41bef2410987d389ec0950becd153
choccho <shocho0101@hotmail.co.jp>
2020-11-07 20:31:14 +0900

    cellのファイルを作った

---
+++ spajam-final-product/Shared/View/MenuTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuTableViewCell.swift
+//  spajam-final-product
+//
+//  Created by 張翔 on 2020/11/07.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
