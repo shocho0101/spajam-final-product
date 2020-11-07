2fb04aa708ec70562cb614706b9d71da55f5b427
fummicc1 <fumiya.tennis1@gmail.com>
2020-11-07 21:58:06 +0900

    Fix PayService.

--- spajam-final-product/Shared/Pay/PayService.swift
+++ spajam-final-product/Shared/Pay/PayService.swift
@@ -29,7 +29,7 @@ final class PayService: NSObject {
         return PKPaymentAuthorizationViewController.canMakePayments(usingNetworks: paymentNetworkToSupport)
     }
     
-    private func buildPaymentRequest(menu: Menu) -> PKPaymentRequest {
+    private func buildPaymentRequest(items: [(menu: Menu, count: Int)]) -> PKPaymentRequest {
         let merchentID = "merchant.com.litech.spajam-final-product.aa"
         let request = PKPaymentRequest()
         request.countryCode = "JP"
@@ -38,14 +38,15 @@ final class PayService: NSObject {
         request.supportedNetworks = paymentNetworkToSupport
         request.merchantCapabilities = .capability3DS
         
-        let item = PKPaymentSummaryItem(label: menu.menuName, amount: 1)
-        request.paymentSummaryItems = [item]
-
+        let items = items.map { (menu, count) in
+            PKPaymentSummaryItem(label: menu.menuName, amount: NSDecimalNumber(value: count))
+        }
+        request.paymentSummaryItems = items
         return request
     }
     
-    func showPaymentViewController(on viewController: UIViewController, menu: Menu) {
-        guard let paymentController = PKPaymentAuthorizationViewController(paymentRequest: buildPaymentRequest(menu: menu)) else {
+    func showPaymentViewController(on viewController: UIViewController, items: [(menu: Menu, count: Int)]) {
+        guard let paymentController = PKPaymentAuthorizationViewController(paymentRequest: buildPaymentRequest(items: items)) else {
             return
         }
         paymentController.delegate = self
