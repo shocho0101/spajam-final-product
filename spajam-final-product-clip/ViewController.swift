ff738f47eb9554ebbdb2f4b1f61d857b329683d2
fummicc1 <fumiya.tennis1@gmail.com>
2020-11-07 16:32:36 +0900

    Add App Clip. Pay

---
+++ spajam-final-product-clip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spajam-final-product-clip
+//
+//  Created by Fumiya Tanaka on 2020/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
