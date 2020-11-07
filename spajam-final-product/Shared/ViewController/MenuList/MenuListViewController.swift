73c9a124ce813d032a2090f183d97cfba1601400
choccho <shocho0101@hotmail.co.jp>
2020-11-07 19:44:15 +0900

    xibをsharedに移した

---
+++ spajam-final-product/Shared/ViewController/MenuList/MenuListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuListViewController.swift
+//  spajam-final-product
+//
+//  Created by 張翔 on 2020/11/07.
+//
+
+import UIKit
+
+class MenuListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
